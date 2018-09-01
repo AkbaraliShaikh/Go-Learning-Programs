34a1f43684b0e8f7ab5738106b6de7dab13f5c17
AkbaraliShaikh <aashaikh55@gmail.com>
2018-09-02 02:37:37 +0530

    pattern, big ints and package test

---
+++ package-test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"pack1"
+)
+
+func main() {
+	fmt.Println(pack1.ReturnStr())
+	fmt.Println(pack1.Pack1Int)
+}
---
+++ src/pack1/pack1.go
@@ -0,0 +1,9 @@
+package pack1
+
+// Pack1Int test package variable
+var Pack1Int int32 = 55
+var pack1Float float32 = 3.14
+
+func ReturnStr() string {
+	return "Hello main!"
+}
